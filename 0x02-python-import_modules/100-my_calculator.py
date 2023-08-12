b6bcac833a7c73fbc6745d4b8a52c95623c7502c
Motunrayo <ajirotutu4real@gmail.com>
2023-08-12 07:01:53 +0000

    Modified

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,7 +16,7 @@ elif argv[2] == '-':
     print("{0} - {1} = {2}".format(a, b, sub(a, b)))
 elif argv[2] == '*':
     print("{0} * {1} = {2}".format(a, b, mul(a, b)))
-elif argv[2] == '\\':
+elif argv[2] == '/':
     print("{0} / {1} = {2}".format(a, b, div(a, b)))
 else:
     print("Unknown operator. Available operators: +, -, * and /")
