b37bf2d6dc2b2347cedb049535ba3870eb0a3517
Motunrayo <ajirotutu4real@gmail.com>
2023-08-14 17:48:24 +0000

    Added files

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin//python3
+def delete_at(my_list=[], idx=0):
+    #Delete item at a specific location
+    if 0 > idx > len(my_list):
+        return my_list
+    del my_list[idx]
+    return my_list
