6df145a3289eb9b4e4cf1c433fe8c528259e15c7
Motunrayo <ajirotutu4real@gmail.com>
2023-08-14 17:51:20 +0000

    Modified

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
     Return a tuple with the length and its first character
     sentence: Input list
     """
-    if len(sentence):
+    if len(sentence) == 0:
         res = (0, None)
     res = (len(sentence), sentence[0])
     return (res)
