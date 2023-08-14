63382f564977969a2c02193b27813eadbafe4549
Motunrayo <ajirotutu4real@gmail.com>
2023-08-14 17:54:32 +0000

    Modified

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,5 @@
-#!/usr/bin//python3
+#!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    # Delete item at a specific location
     if 0 > idx > len(my_list):
         return my_list
     del my_list[idx]
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -6,5 +6,6 @@ def multiple_returns(sentence):
     """
     if len(sentence) == 0:
         res = (0, None)
-    res = (len(sentence), sentence[0])
+    else:
+        res = (len(sentence), sentence[0])
     return (res)
