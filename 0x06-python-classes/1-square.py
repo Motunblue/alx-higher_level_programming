a9a8445b0cf3fb55d51ed24cff6a336480b2d041
Motunrayo <ajirotutu4real@gmail.com>
2023-08-29 17:53:24 +0000

    Added files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Define a Square class """
+
+
+class Square:
+    """A square
+
+    Attributes:
+       __size (int): size of square
+    """
+    def __init__(self, size):
+        """Square initialization
+
+        Args:
+           size (int): square size
+
+        """
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+""" Define a Square class """
+
+
+class Square:
+    """A square
+
+    Attributes:
+       __size (int): size of square
+    """
+    def __init__(self, size=0):
+        """Square initialization
+
+        Args:
+           size (int): square size
+
+        """
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be an integer")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Area of a square
+
+        Returns:
+           square area
+
+        """
+        return self.__size * self.__size
