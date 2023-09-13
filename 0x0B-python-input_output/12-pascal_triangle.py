0f30c2b1698a34854dff8c0f74000f272ee86e7b
Motunrayo <ajirotutu4real@gmail.com>
2023-09-13 18:37:11 +0000

    Added 12 pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/ptyhon3
+"""Pascal triangle Module
+"""
+
+
+def pascal_triangle(n):
+    """Returns list of integers representing Pascal triangle of n
+    """
+
+    mylist = [[1]]
+    if n <= 0:
+        return []
+
+    for i in range(n - 1):
+        tmp = [1]
+        for j in range(i):
+            tmp.append(mylist[-1][j] + mylist[-1][j + 1])
+        tmp.append(1)
+        mylist.append(tmp)
+
+    return mylist
