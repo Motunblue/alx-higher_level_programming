413d6300ebd57e4d57055ee2e2cece27bb04fd4f
Motunrayo <ajirotutu4real@gmail.com>
2023-10-09 08:25:38 +0000

    Added display method to rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,3 +80,11 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Returns area of the retangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """Prints rectangle instace with chr #"""
+        print("\n".join(["#" * self.__width] * self.__height))
