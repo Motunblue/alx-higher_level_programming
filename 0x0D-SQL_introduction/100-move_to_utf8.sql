6253ead1389aba7c040bac57be3665265b86eec3
Motunrayo <ajirotutu4real@gmail.com>
2023-10-17 21:50:01 +0000

    Modified

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,6 @@
 -- Convert the hbtn_0c_0 database to UTF8
-USE hbtn_0c_0 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CHANGE name name VARCHAR(255)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
