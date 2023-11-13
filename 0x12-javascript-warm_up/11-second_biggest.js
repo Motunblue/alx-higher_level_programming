773df85d072042aea61e2a3b1ba3dec547cb5d89
Motun <ajirotutu4real@gmail.com>
2023-11-14 00:50:22 +0100

    Modified 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,10 @@ const argv = process.argv;
 if (argv.length <= 3) {
   console.log('0');
 } else {
-  let largest = +argv[2];
-  let secondLargest = +argv[3];
+  let largest = +argv[3];
+  let secondLargest = +argv[2];
   argv.forEach((val) => {
-    if (+val >= largest) {
+    if (+val > largest) {
       secondLargest = largest;
       largest = +val;
     }
