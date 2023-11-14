967571169e24e9ea3ca63a119a80cbfedb3b755a
Motunblue <ajirotutu4real@gmail.com>
2023-11-14 21:50:37 +0000

    Modified 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareParent = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends SquareParent {
   charPrint (c) {
-    if (c !== undefined) {
-      for (let i = 0; i < this.width; i++) {
-        console.log(c.repeat(this.height));
-      }
-    } else {
-      this.print();
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.height));
     }
   }
 }
