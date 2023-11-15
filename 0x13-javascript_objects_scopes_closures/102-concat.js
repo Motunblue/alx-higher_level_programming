5ad3cfc823f67cac3b2dae53e44bc9f04b4058f5
Motun <ajirotutu4real@gmail.com>
2023-11-15 22:34:47 +0100

    added 102-concat

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const argv = process.argv;
+
+fs.readFile(argv[2], 'utf8', (err, data1) => {
+  if (err) {
+    console.error(`Error reading ${argv[2]}: ${err}`);
+    return;
+  }
+  fs.readFile(argv[3], 'utf8', (err, data2) => {
+    if (err) {
+      console.error(`Error reading ${argv[3]}: ${err}`);
+      return;
+    }
+    const concat = data1 + data2;
+    fs.writeFile(argv[4], concat, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to ${argv[4]}: ${err}`);
+        return;
+      }
+    });
+  });
+});
