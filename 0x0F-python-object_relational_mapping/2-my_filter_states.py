95f9025f514c50d4d7a2d626847628409fa5b9c7
Motun <ajirotutu4real@gmail.com>
2023-11-18 09:47:44 +0100

    Added 1 & 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    Select States module
+"""
+
+
+def connect(username, passwd, dbname):
+    """Creates a database connection
+    """
+    conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=passwd,
+        db=dbname
+    )
+    return conn
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    """Select states table from a database
+    """
+    conn = connect(sys.argv[1], sys.argv[2], sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s \
+                ORDER BY id ASC", (sys.argv[4],))
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    conn.close()
