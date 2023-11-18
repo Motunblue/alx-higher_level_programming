7295362b06d0393aedd0c66ad7e1f570232c1176
Motun <ajirotutu4real@gmail.com>
2023-11-18 08:48:50 +0100

    Added ORM

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+"""
+    Select States module
+"""
+
+
+def connect(username, passwd, dbname):
+    """Creates a database connection
+    """
+    conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=passwd,
+        db=dbname
+    )
+    return conn
+
+
+if __name__ == "__main__":
+    """Select states table from a database
+    """
+    conn = connect(sys.argv[1], sys.argv[2], sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    conn.close()
