9994d2fcc66174252df73a1ad849a2ce26da7462
Motun <ajirotutu4real@gmail.com>
2023-12-02 17:22:06 +0100

    Added task 5

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,10 +11,13 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         letter = sys.argv[1]
     r = requests.post("http://0.0.0.0:5000/search_user", {"q": letter})
-    dic = eval(r.text)
-    if not isinstance(dic, dict):
+
+    try:
+        res = r.json()
+        if not res:
+            print("No result")
+        else:
+            print("[{}] {}".format(res["id"], res["name"]))
+    except ValueError:
         print("Not a valid JSON")
-    elif not dic:
-        print("No result")
-    else:
-        print("[{}] {}".format(dic["id"], dic["name"]))
+    
